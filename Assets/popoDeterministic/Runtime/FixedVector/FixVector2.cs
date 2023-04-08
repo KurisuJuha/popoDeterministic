5b103ad00a53205a3ea0f14734ef23a0288f25e6
KYapp <kukiyomenaisapp@gmail.com>
2023-04-08 23:06:01 +0900

    up

--- Assets/popoDeterministic/Runtime/FixedVector/FixVector2.cs
+++ Assets/popoDeterministic/Runtime/FixedVector/FixVector2.cs
@@ -27,7 +27,9 @@ namespace JuhaKurisu.PopoTools.Deterministics
 
         private static readonly FixVector2 zeroVector = new(new(0), new(0));
         private static readonly FixVector2 oneVector = new(new(1), new(1));
+        private static readonly FixVector2 upVector = new(new(0), new(1));
         public static FixVector2 zero => zeroVector;
         public static FixVector2 one => oneVector;
+        public static FixVector2 up => upVector;
     }
 }
