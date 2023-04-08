cb7326034cabf00aa2dca71f6d17c4b6309a85a4
KYapp <kukiyomenaisapp@gmail.com>
2023-04-08 23:05:46 +0900

    indexでアクセスできるようにした

--- Assets/popoDeterministic/Runtime/FixedVector/FixVector2.cs
+++ Assets/popoDeterministic/Runtime/FixedVector/FixVector2.cs
@@ -7,6 +7,20 @@ namespace JuhaKurisu.PopoTools.Deterministics
         public Fix64 x { get; private set; }
         public Fix64 y { get; private set; }
 
+        public Fix64 this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0: return x;
+                    case 1: return y;
+                    default:
+                        throw new IndexOutOfRangeException("Invalid Vector2 index!");
+                }
+            }
+        }
+
         public FixVector2(Fix64 x, Fix64 y) { this.x = x; this.y = y; }
 
         public FixVector2(Fix64 x) { this.x = x; this.y = Fix64.Zero; }
