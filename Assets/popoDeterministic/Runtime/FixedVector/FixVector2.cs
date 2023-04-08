a92d43b7c5e1e60d24bb007dce5436a153ca7194
KYapp <kukiyomenaisapp@gmail.com>
2023-04-08 23:05:30 +0900

    xとyをセット

--- Assets/popoDeterministic/Runtime/FixedVector/FixVector2.cs
+++ Assets/popoDeterministic/Runtime/FixedVector/FixVector2.cs
@@ -4,5 +4,8 @@ namespace JuhaKurisu.PopoTools.Deterministics
 {
     public struct FixVector2
     {
+        public Fix64 x { get; private set; }
+        public Fix64 y { get; private set; }
+
     }
 }
