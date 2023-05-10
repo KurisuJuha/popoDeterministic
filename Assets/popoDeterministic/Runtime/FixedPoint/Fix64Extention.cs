8e804320bab2818a0bd2d6f8ec47da795be090ef
Juha <kukiyomenaisapp@gmail.com>
2023-05-10 13:28:20 +0900

    FastAbs

--- Assets/popoDeterministic/Runtime/FixedPoint/Fix64Extention.cs
+++ Assets/popoDeterministic/Runtime/FixedPoint/Fix64Extention.cs
@@ -8,5 +8,8 @@ namespace JuhaKurisu.PopoTools.Deterministics.Extentions
         public static Fix64 Abs(this Fix64 value)
             => Fix64.Abs(value);
 
+        public static Fix64 FastAbs(this Fix64 value)
+            => Fix64.FastAbs(value);
+
     }
 }
