355ab0e4d6749700d7288f6bc5f7ef05d45aa452
Juha <kukiyomenaisapp@gmail.com>
2023-05-10 13:28:09 +0900

    Sign

--- Assets/popoDeterministic/Runtime/FixedPoint/Fix64Extention.cs
+++ Assets/popoDeterministic/Runtime/FixedPoint/Fix64Extention.cs
@@ -2,5 +2,8 @@ namespace JuhaKurisu.PopoTools.Deterministics.Extentions
 {
     public static class Fix64Extention
     {
+        public static int Sign(this Fix64 value)
+            => Fix64.Sign(value);
+
     }
 }
